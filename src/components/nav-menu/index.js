f360dcdc814b75942e80167c6e1a7577c003c3cf
Chris Troutner <chris.troutner@gmail.com>
2022-11-02 13:30:27 -0700

    Got initial functionality in place

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -7,7 +7,8 @@
 
 // Global npm libraries
 import React from 'react'
-import { Nav, Navbar, Image } from 'react-bootstrap'
+// import { Nav, Navbar, Image } from 'react-bootstrap'
+import { Navbar, Image } from 'react-bootstrap'
 import Logo from './psf-logo.png'
 
 class NavMenu extends React.Component {
@@ -22,22 +23,23 @@ class NavMenu extends React.Component {
     // console.log(`selectedMenuItem: ${this.state.selectedMenuItem}`)
     // this.handleMenuClick(this.state.selectedMenuItem)
 
+    // <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+    // <Navbar.Collapse id='responsive-navbar-nav'>
+    //   <Nav className='mr-auto'>
+    //     <Nav.Link href='#' onClick={() => this.handleClickEvent(0)}>Check Balance</Nav.Link>
+    //     <Nav.Link href='#' onClick={() => this.handleClickEvent(1)}>Placeholder2</Nav.Link>
+    //     <Nav.Link href='#' onClick={() => this.handleClickEvent(2)}>Placeholder 3</Nav.Link>
+    //   </Nav>
+    // </Navbar.Collapse>
+
     return (
       <>
         <Navbar collapseOnSelect expand='xxxl' bg='dark' variant='dark' style={{ paddingRight: '20px' }}>
           <Navbar.Brand href='#home' style={{ paddingLeft: '20px' }}>
             <Image src={Logo} thumbnail width='50' />{' '}
-            PSF Web3 Demo
+            P2WDB BCH Explorer
           </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='mr-auto'>
-              <Nav.Link href='#' onClick={() => this.handleClickEvent(0)}>Check Balance</Nav.Link>
-              <Nav.Link href='#' onClick={() => this.handleClickEvent(1)}>Placeholder2</Nav.Link>
-              <Nav.Link href='#' onClick={() => this.handleClickEvent(2)}>Placeholder 3</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
         </Navbar>
       </>
     )
---
+++ src/components/p2wdb-explorer/index.js
@@ -0,0 +1,154 @@
+/*
+  Component for looking up the balance of a BCH address.
+*/
+
+// Global npm libraries
+import React from 'react'
+import { Container, Row, Col, Table, Card } from 'react-bootstrap'
+import axios from 'axios'
+import { DatatableWrapper, TableBody, TableHeader } from 'react-bs-datatable'
+
+const SERVER = 'https://p2wdb.fullstack.cash/'
+
+const TABLE_HEADERS = [
+  {
+    prop: 'createdAt',
+    title: 'Created At',
+    isFilterable: true
+  },
+  {
+    prop: 'txid',
+    title: 'Transaction'
+  },
+  {
+    prop: 'hash',
+    title: 'Hash'
+  },
+  {
+    prop: 'appId',
+    title: 'App ID'
+  }
+]
+
+class P2WDBExplorer extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      balance: '',
+      textInput: '',
+      wallet: props.wallet,
+      endpoint: `${SERVER}entry/all/${0}`,
+      data: []
+    }
+
+    // Encapsulate dependencies
+    this.axios = axios
+
+    // Bind 'this' to event handlers
+    this.handleEntries = this.handleEntries.bind(this)
+    this.getEntries = this.getEntries.bind(this)
+
+    // _this = this
+  }
+
+  render () {
+    return (
+
+      <>
+        <Container>
+          <Row>
+            <Col className='text-break' style={{ textAlign: 'center' }}>
+              <Card>
+                <Card.Title>Lastest P2WDB Entries</Card.Title>
+
+                <DatatableWrapper body={this.state.data} headers={TABLE_HEADERS}>
+                  <Table>
+                    <TableHeader />
+                    <TableBody />
+                  </Table>
+                </DatatableWrapper>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+  }
+
+  async componentDidMount () {
+    this.handleEntries()
+
+    // Get data and update table
+    // every 20 seconds
+    setInterval(this.handleEntries, 20000)
+  }
+
+  // Top-level orchestration function.
+  async handleEntries () {
+    const entries = await this.getEntries()
+    this.generateDataTable(entries)
+  }
+
+  // REST petition to Get data fron the pw2db
+  async getEntries (endpoint) {
+    try {
+      const url = endpoint || this.state.endpoint
+
+      // console.log(`url : ${url}`)
+      const options = {
+        method: 'GET',
+        url,
+        data: {}
+      }
+      const result = await this.axios.request(options)
+
+      this.setState({
+        entries: result.data.data,
+        inFetch: false
+      })
+
+      return result.data.data
+    } catch (err) {
+      console.warn('Error in getEntries() ', err)
+    }
+  }
+
+  // Generate table content
+  generateDataTable (dataArr) {
+    try {
+      const data = []
+
+      for (let i = 0; i < dataArr.length; i++) {
+        const entry = dataArr[i]
+        const row = {
+          // createdAt row data
+          createdAt: new Date(entry.createdAt).toLocaleString(),
+          txid: (<a href={`https://blockchair.com/bitcoin-cash/transaction/${entry.key}`} target='_blank' rel='noreferrer'>{this.cutString(entry.key)}</a>),
+          hash: (<a href={`https://p2wdb.fullstack.cash/entry/hash/${entry.hash}`} target='_blank' rel='noreferrer'>{this.cutString(entry.hash)}</a>),
+          appId: entry.appId || 'none'
+        }
+        data.push(row)
+      }
+      console.log('data: ', data)
+
+      this.setState({
+        data
+      })
+    } catch (err) {
+      console.warn('Error in generateDataTable() ', err)
+    }
+  }
+
+  cutString (txid) {
+    try {
+      const subTxid = txid.slice(0, 4)
+      const subTxid2 = txid.slice(-4)
+      return `${subTxid}...${subTxid2}`
+    } catch (err) {
+      console.warn('Error in cutString() ', err)
+    }
+  }
+}
+
+export default P2WDBExplorer
